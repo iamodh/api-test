e641e13b9f7576647fe90914d5eb995e9c0cb192
Donghwan Oh <imodh97@gmail.com>
2024-04-26 13:27:51 +0900

    get beach weather forecast & water temperature

--- libs/getBeachWeather.js
+++ libs/getBeachWeather.js
@@ -1,46 +1,47 @@
 const BASE_URL = "http://apis.data.go.kr/1360000/BeachInfoservice";
 const API_KEY1 =
-  "SKyFFJUUiEqNgxwXDES6CgAtpZbzDm5VSOOchabfhloEfPdFje2y0PmEGdoggoE3mBY3dHKBfSrN%2BpX3EeI49A%3D%3D ";
+  "SKyFFJUUiEqNgxwXDES6CgAtpZbzDm5VSOOchabfhloEfPdFje2y0PmEGdoggoE3mBY3dHKBfSrN%2BpX3EeI49A%3D%3D";
 const API_KEY2 =
   "SKyFFJUUiEqNgxwXDES6CgAtpZbzDm5VSOOchabfhloEfPdFje2y0PmEGdoggoE3mBY3dHKBfSrN+pX3EeI49A==";
-const DATATYPE = "JSON";
-const BEACHID = ["304", "305", "306", "307", "308", "309"]; // 해운대, 송정, 광안리, 임랑, 다대포, 일광
-const numOfRows = 10;
+const DATA_TYPE = "JSON";
+const BEACH_ID = ["304", "305", "306", "307", "308", "309"]; // 해운대, 송정, 광안리, 임랑, 다대포, 일광
+const NUM_OF_ROWS = 10;
+
 /* Get date */
 const offset = new Date().getTimezoneOffset() * 60000;
 const today = new Date(Date.now() - offset);
 console.log(today.toISOString()); // 2024-04-24T12:38:37.603Z
 const BASE_DATE = today.toISOString().substring(0, 10).replaceAll("-", ""); // 20240424
-const BASE_TIME = today.toISOString().substring(11, 16).replaceAll(":", ""); // 1238
 
-const SEARCHTIME = BASE_DATE + BASE_TIME;
+let BASE_TIME;
+const base = ["0200", "0500", "0800", "1100", "1400", "1700", "2000", "2300"];
+const time = +today.toISOString().substring(11, 16).replaceAll(":", ""); // 1238 (number)
+// 현재 시간에 맞춰 api 제공 BASE_TIME 갱신
+base.forEach((base) => {
+  if (time > +base) {
+    BASE_TIME = base;
+  }
+});
 
 /* Info Params */
 // 단기예보조회
 // fixed params: serviceKey, dataType, numOfRows,
 // non-fixed params: base_date, base_time, beach_num
 const FORECAST = "/getVilageFcstBeach";
+export function getBeachForecast() {
+  return fetch(
+    `${BASE_URL}${FORECAST}?serviceKey=${API_KEY1}&dataType=${DATA_TYPE}&numOfRows=${NUM_OF_ROWS}&beach_num=304&base_time=${BASE_TIME}&base_date=${BASE_DATE}`
+  ).then((response) => response.json());
+}
 
 // 해수욕장 수온조회
 // fixed params: serviceKey, dataType, numOfRows,
-// non-fixed params: search_time, beach_num
+// non-fixed params: searchTime, beach_num
 const WATERTEMP = "/getTwBuoyBeach";
+const SEARCHTIME = BASE_DATE + BASE_TIME;
 
-// 해수욕장 일출일몰조회
-// fixed params: serviceKey, dataType, numOfRows,
-// non-fixed params: base_date, beach_num
-const SUNINFO = "/getSunInfoBeach";
-
-// 해수욕장 조석정보 (6-8월 이용 가능)
-// fixed params: serviceKey, dataType, numOfRows,
-// non-fixed params: base_date, beach_num
-const TIDEINFO = "/getTideInfoBeach";
-
-// 해수욕장 파고조회
-// fixed params: serviceKey, dataType, numOfRows,
-// non-fixed params: search_time, beach_num
-const WAVEHEIGHT = "/getWhBuoyBeach";
-
-export function getBeachWeather() {
-  return fetch();
+export function getBeachTemp() {
+  return fetch(
+    `${BASE_URL}${WATERTEMP}?serviceKey=${API_KEY1}&dataType=${DATA_TYPE}&numOfRows=${NUM_OF_ROWS}&beach_num=304&searchTime=${SEARCHTIME}`
+  ).then((response) => response.json());
 }
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
 import { getWeather } from "../libs/getWeather";
 import { useQuery } from "react-query";
+import { getBeachForecast, getBeachTemp } from "../libs/getBeachWeather";
 
 export default function Weather() {
   const { isLoading: weatherLoading, data: weatherData } = useQuery(
-    ["weatherInfo"],
+    ["weather", "busan"],
     getWeather
   );
-  const { isLoading: beachWeatherLoading, data: beachWeatherData } = useQuery(
-    ["beachWeatherInfo"],
-    getBeachWeather
+  const { isLoading: beachForecastLoading, data: beachForecastData } = useQuery(
+    ["weather", "beachForecast"],
+    getBeachForecast
+  );
+  const { isLoading: beachTempLoading, data: beachTempData } = useQuery(
+    ["weather", "beachTemp"],
+    getBeachTemp
   );
 
   return (
@@ -27,6 +32,22 @@ export default function Weather() {
             src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
           />
           <p>{JSON.stringify(weatherData)}</p>
+        </>
+      )}
+      {beachForecastLoading ? (
+        "Loading..."
+      ) : (
+        <>
+          <h2>해수욕장 단기 예보 (해운대)</h2>
+          <p>{JSON.stringify(beachForecastData)}</p>
+        </>
+      )}
+      {beachTempLoading ? (
+        "Loading..."
+      ) : (
+        <>
+          <h2>해수욕장 수온</h2>
+          <p>{JSON.stringify(beachTempData)}</p>
         </>
       )}
     </>
