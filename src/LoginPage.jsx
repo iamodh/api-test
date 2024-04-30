ea6af382db0330438da71eeab09aec28621ad61f
mongzino <112944480+mongzino@users.noreply.github.com>
2024-05-01 08:55:39 +0900

    testing metadata

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -5,6 +5,33 @@ import { useState, useEffect } from "react";
 export default function Login() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loggedUser, setLoggedUser] = useState(null);
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+  const [userName, setUserName] = useState("");
+
+  async function userLogin() {
+    let { data, error } = await supabase.auth.signInWithPassword({
+      email: loginEmail,
+      password: loginPassword,
+    });
+    if (error) console.log(error);
+  }
+
+  async function userSignUp() {
+    let { data, error } = await supabase.auth.signUp({
+      email: signUpEmail,
+      password: signUpPassword,
+      user_metadata: {
+        user_name: userName,
+        avatar_url:
+          "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+      },
+    });
+    if (error) console.log(error);
+    console.log(data);
+  }
 
   async function googleLogin() {
     let { data, error } = await supabase.auth.signInWithOAuth({
@@ -27,14 +54,15 @@ export default function Login() {
 
   async function checkLogin() {
     const authInfo = await supabase.auth.getSession();
-    ``;
     const session = authInfo.data.session;
     if (session) {
       const {
         data: { user },
+        error,
       } = await supabase.auth.getUser();
       setLoggedUser(user);
     }
+    if (authInfo.error) console.log(authInfo.error);
     console.log(loggedUser);
     setIsLoggedIn(!!session);
   }
@@ -43,7 +71,6 @@ export default function Login() {
     const { error } = await supabase.auth.signOut();
     checkLogin();
   }
-
   useEffect(() => {
     checkLogin();
   }, []);
@@ -52,6 +79,56 @@ export default function Login() {
       <div className="login-box">
         {!isLoggedIn && (
           <>
+            <form className="userLoginBox" onSubmit={(e) => e.preventDefault()}>
+              <input
+                className="loginEmail"
+                type="text"
+                placeholder="email"
+                name="email"
+                value={loginEmail}
+                onChange={(e) => setLoginEmail(e.target.value)}
+              ></input>
+              <input
+                className="loginPassword"
+                type="password"
+                placeholder="password"
+                name="password"
+                value={loginPassword}
+                onChange={(e) => setLoginPassword(e.target.value)}
+              ></input>
+              <button className="loginBtn" onClick={userLogin}>
+                로그인
+              </button>
+            </form>
+            <form className="signUpBox" onSubmit={(e) => e.preventDefault()}>
+              <input
+                className="signUpEmail"
+                type="text"
+                placeholder="email"
+                name="email"
+                value={signUpEmail}
+                onChange={(e) => setSignUpEmail(e.target.value)}
+              ></input>
+              <input
+                className="signUpPassword"
+                type="password"
+                placeholder="password"
+                name="password"
+                value={signUpPassword}
+                onChange={(e) => setSignUpPassword(e.target.value)}
+              ></input>
+              <input
+                className="userName"
+                type="text"
+                placeholder="이름"
+                name="userName"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              ></input>
+              <button className="signUpBtn" onClick={userSignUp}>
+                회원가입
+              </button>
+            </form>
             <button className="google" onClick={googleLogin}>
               구글 로그인
             </button>
@@ -65,11 +142,9 @@ export default function Login() {
             <button className="logOut" onClick={signOut}>
               로그아웃
             </button>
-            <div>
-              <img src={loggedUser.user_metadata.avatar_url} alt="avatar" />
-              <p>user name : {loggedUser.user_metadata.full_name}</p>
-              <p>user email : {loggedUser.user_metadata.email}</p>
-            </div>
+            <p>{loggedUser.email}</p>
+            <p>{loggedUser.user_metadata.user_name}</p>
+            <img src={loggedUser.user_metadata.avatar_url} alt="" />
           </>
         )}
       </div>
